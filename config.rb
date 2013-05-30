272b164e1741d152e1d2548799cbf274ad6101ed
Mark Reilly <mark@alienresident.net>
2013-05-29 23:18:57 -0400

    Added compass-pattern-primer extension depenency

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
+require 'compass-pattern-primer'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -7,7 +9,6 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
@@ -15,7 +16,7 @@ javascripts_dir = "javascripts"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
- line_comments = false
+# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
